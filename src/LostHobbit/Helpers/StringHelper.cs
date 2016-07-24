7e7b26e4daaa57c20a2abed818c9eef8c5eca16f
Stephen_2 <Stephen_2@I3-LAPTOP>
2016-07-24 17:32:59 +0100

    Added helper classes.

---
+++ src/LostHobbit/Helpers/StringHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LostHobbit.Helpers
+{
+    public static class StringHelper
+    {
+        public static string Repeat(this string stringToRepeat, int count)
+        {
+            return String.Concat(Enumerable.Range(0, count).Select(x => stringToRepeat));
+        }
+
+        public static string Spaces(int numberOfSpaces)
+        {
+            return new String(' ', numberOfSpaces);
+        }
+
+        /// <summary>
+        /// Prefixes with a or an.
+        /// </summary>
+        public static string PrefixIndefiniteArticle(this string noun)
+        {
+            return ("aeiou".Contains(Char.ToLower(noun[0])) ? "an" : "a") + ' ' + noun;
+        }
+
+        public static string CapitalizeFirstLetter(this string text)
+        {
+            return Char.ToUpper(text[0]) + text.Substring(1);
+        }
+
+        /// <summary>
+        /// Converts camel case into end user readable text.
+        /// </summary>
+        public static string CamelCaseToSpacedWords(this string camelCase)
+        {
+            return Regex.Replace(
+                Regex.Replace(
+                    camelCase,
+                    @"(\P{Ll})(\P{Ll}\p{Ll})",
+                    "$1 $2"
+                ),
+                @"(\p{Ll})(\P{Ll})",
+                "$1 $2"
+            );
+        }
+    }
+
+}
